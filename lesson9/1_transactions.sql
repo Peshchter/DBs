462f0f93c4d2c871dccfa8fc5b877580034af616
Peshchter <45987528+Peshchter@users.noreply.github.com>
2019-11-06 19:51:03 +0400

    Часть 1: Транзакции и представления.

--- lesson9/1_transactions.sql
+++ lesson9/1_transactions.sql
@@ -1,1 +1,25 @@
-//просьба пока не проверять
+/* В базе данных shop и sample присутствуют одни и те же таблицы, учебной базы данных. 
+ * Переместите запись id = 1 из таблицы shop.users в таблицу sample.users. 
+ * Используйте транзакции.
+ */
+
+start transaction;
+insert into sample.users 
+	select * from shop.users 
+	where id = 1;
+delete from shop.users 
+	where id =1;
+commit;
+
+/* Создайте представление, которое выводит название name 
+ * товарной позиции из таблицы products и 
+ * соответствующее название каталога name из таблицы catalogs.
+ *  
+ * */
+
+create view cat_v as
+	select products.name as `name`, catalogs.name as `catalog`	
+	from products, catalogs
+	where products.catalog_id = catalogs.id;
+
+
